9813693954229a3ac90800b61d37c8aee26bb675
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-09 01:57:43 +0530

    Update page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -54,7 +54,7 @@ const MyProfile = ()=>{
     return(
         <div>
             <Profile 
-                name='my'
+                name='Your'
                 desc='Welcome to your personalized profile page'
                 data={posts}
                 handleEdit={handleEdit}
