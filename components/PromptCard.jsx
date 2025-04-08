72d1a8d258b221d6d298d2dcabceb890879e8411
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-09 01:48:47 +0530

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -53,7 +53,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
               height={12}
             />
           </div>
-       </div>``
+       </div>
        <p className='my-4 font-satoshi text-sm text-gray-700'>
         {post.prompt}
        </p>
@@ -61,7 +61,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
        onClick={()=> handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
        {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
-          <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>edit</p>
+          <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>Edit</p>
           <p className='font-inter text-sm orange_gradient cursor-pointer ' onClick={handleDelete}>Delete</p>
 
           </div>
