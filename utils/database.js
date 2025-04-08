2109a3d65dbfd4f07b6b94b08384263052c17bb8
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-09 01:30:11 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set("strictQuery", true);
+    // mongoose.set("strictQuery", true);
 
     if (isConnected) {
         console.log("MongoDb is already connected");
@@ -15,8 +15,8 @@ export const connectToDB = async () => {
             dbName: "share_prompt",
             serverSelectionTimeoutMS: 30000, // Increase timeout
             socketTimeoutMS: 45000,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
 
         isConnected = true;
